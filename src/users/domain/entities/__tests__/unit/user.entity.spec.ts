a56565a7546a8d5efeca4e2c9219a30ae4762b21
= <cfelipeconsorte2001@gmail.com>
2025-06-13 12:28:22 -0300

    UserEntity - getters and methods unit tests

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -18,4 +18,24 @@ describe('userEntity unit tests', () => {
     expect(sut.props.email).toEqual(props.email)
     expect(sut.props.createdAt).toBeInstanceOf(Date)
   })
+  it('getter of name field', () => {
+    expect(sut.props.name).toBeDefined()
+    expect(sut.props.name).toEqual(props.name)
+    expect(typeof sut.props.name).toEqual('string')
+  })
+  it('getter of passwors field', () => {
+    expect(sut.props.password).toBeDefined()
+    expect(sut.props.password).toEqual(props.password)
+    expect(typeof sut.props.password).toEqual('string')
+  })
+  it('getter of email field', () => {
+    expect(sut.props.email).toBeDefined()
+    expect(sut.props.email).toEqual(props.email)
+    expect(typeof sut.props.email).toEqual('string')
+  })
+
+  it('getter of createAt field', () => {
+    expect(sut.props.createdAt).toBeDefined()
+    expect(sut.props.createdAt).toBeInstanceOf(Date)
+  })
 })
