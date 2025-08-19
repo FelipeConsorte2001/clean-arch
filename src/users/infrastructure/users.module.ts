ba5fb8a78b785603839ef67e2f2513fdd688dbf0
= <cfelipeconsorte2001@gmail.com>
2025-08-19 19:29:15 -0300

    Registering SingIn user case

--- src/users/infrastructure/users.module.ts
+++ src/users/infrastructure/users.module.ts
@@ -1,9 +1,34 @@
+import { HashProvider } from '@/shared/application/providers/hash-provider'
 import { Module } from '@nestjs/common'
+import { SignupUseCase } from '../application/usecase/sign-up.usecase'
+import { UserRepository } from '../domain/repositories/user.repository'
+import { UserInMemoryRepository } from './database/in-memory/repositories/user-in-memory.repository'
+import { bcryptjsHashProvider } from './providers/hash-provider/bcryptjs-hash.provider'
 import { UsersController } from './users.controller'
 import { UsersService } from './users.service'
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [
+    UsersService,
+    {
+      provide: 'UserRepository',
+      useClass: UserInMemoryRepository,
+    },
+    {
+      provide: 'HashProvider',
+      useClass: bcryptjsHashProvider,
+    },
+    {
+      provide: SignupUseCase,
+      useFactory: (
+        userRepository: UserRepository,
+        hashProvider: HashProvider,
+      ) => {
+        return new SignupUseCase(userRepository, hashProvider)
+      },
+      inject: ['UserRepository', 'HashProvider'],
+    },
+  ],
 })
 export class UsersModule {}
