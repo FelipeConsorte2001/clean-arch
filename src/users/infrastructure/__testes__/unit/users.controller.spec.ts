9b96e10b4f4ba63a93b06f14a470c831bcfe3b32
= <cfelipeconsorte2001@gmail.com>
2025-08-23 16:24:58 -0300

    usersController unit tests login method

--- src/users/infrastructure/__testes__/unit/users.controller.spec.ts
+++ src/users/infrastructure/__testes__/unit/users.controller.spec.ts
@@ -1,6 +1,8 @@
 import { UserOutput } from '@/users/application/dtos/user-output'
-import { Output } from '@/users/application/usecase/sign-up.usecase'
+import { Output as outputSignIn } from '@/users/application/usecase/sign-in.usecase'
+import { Output as outputSignUp } from '@/users/application/usecase/sign-up.usecase'
 import { v4 } from 'uuid'
+import { SinginDto } from '../../dtos/signin.dto'
 import { SingupDto } from '../../dtos/signup.dto'
 import { UsersController } from '../../users.controller'
 describe('Users Controller unit tests', () => {
@@ -24,7 +26,7 @@ describe('Users Controller unit tests', () => {
     expect(sut).toBeDefined()
   })
   it('Should create a user', async () => {
-    const output: Output = props
+    const output: outputSignUp = props
     const mockSignupUse = {
       execute: jest.fn().mockReturnValue(Promise.resolve(output)),
     }
@@ -37,5 +39,19 @@ describe('Users Controller unit tests', () => {
     const result = await sut.create(input)
     expect(output).toMatchObject(result)
     expect(mockSignupUse.execute).toHaveBeenCalledWith(input)
+  })
+  it('Should authenticate a user', async () => {
+    const output: outputSignIn = props
+    const mockSigninUse = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    }
+    sut['singinUseCase'] = mockSigninUse as any
+    const input: SinginDto = {
+      email: 'a@a.com',
+      password: '1234',
+    }
+    const result = await sut.login(input)
+    expect(output).toMatchObject(result)
+    expect(mockSigninUse.execute).toHaveBeenCalledWith(input)
   })
 })
