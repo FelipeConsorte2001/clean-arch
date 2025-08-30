57afbb2ae5233acfd493612d2ebc6c419c2db9bb
= <cfelipeconsorte2001@gmail.com>
2025-08-30 14:04:44 -0300

    integration test os serach method

--- src/users/infrastructure/database/prisma/repositories/__tests__/integration/user-prisma.repository.int-spec.ts
+++ src/users/infrastructure/database/prisma/repositories/__tests__/integration/user-prisma.repository.int-spec.ts
@@ -2,6 +2,10 @@ import { NotFoundError } from '@/shared/doman/erros/not-found-error'
 import { DatabaseModule } from '@/shared/infrastructure/database/database.module'
 import { setupPrismaTests } from '@/shared/infrastructure/database/prisma/testing/setup-prisma/setup-prisma-tests'
 import { UserEntity } from '@/users/domain/entities/user.entity'
+import {
+  SearchParams,
+  SearchResults,
+} from '@/users/domain/repositories/user.repository'
 import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
 import { Test, TestingModule } from '@nestjs/testing'
 import { PrismaClient } from '@prisma/client'
@@ -56,5 +60,86 @@ describe('UserPrismaRepository integration tests', () => {
     expect(entities).toHaveLength(1)
     expect(JSON.stringify(entities)).toBe(JSON.stringify([entity]))
     entities.map(item => expect(item.toJSON()).toStrictEqual(entity.toJSON()))
+  })
+
+  describe('search method test', () => {
+    it('should apply only pagination when the other params are null', async () => {
+      const createAt = new Date()
+      const entities: UserEntity[] = []
+      const arrage = Array(16).fill(UserDataBuilder({}))
+      arrage.forEach((element, index) => {
+        entities.push(
+          new UserEntity({
+            ...element,
+            email: `test${index}@gmail.com`,
+            createdAt: new Date(createAt.getTime() + index),
+          }),
+        )
+      })
+      await prismaService.user.createMany({
+        data: entities.map(item => item.toJSON()),
+      })
+
+      const searchOutput = await sut.search(new SearchParams())
+      const items = searchOutput.items
+
+      expect(searchOutput).toBeInstanceOf(SearchResults)
+      expect(searchOutput.total).toBe(16)
+      expect(searchOutput.items.length).toBe(15)
+      expect(
+        searchOutput.items.forEach(item =>
+          expect(item).toBeInstanceOf(UserEntity),
+        ),
+      )
+      items.reverse().forEach((items, index) => {
+        expect(`test${index + 1}@gmail.com`).toBe(items.email)
+      })
+    })
+    it('should search using filter, sort and paginate', async () => {
+      const createAt = new Date()
+      const entities: UserEntity[] = []
+      const arrage = ['test', 'a', 'TEST', 'b', 'TeSt']
+
+      arrage.forEach((element, index) => {
+        entities.push(
+          new UserEntity({
+            ...UserDataBuilder({ name: element }),
+            createdAt: new Date(createAt.getTime() + index),
+          }),
+        )
+      })
+      await prismaService.user.createMany({
+        data: entities.map(item => item.toJSON()),
+      })
+
+      const searchOutputPage1 = await sut.search(
+        new SearchParams({
+          page: 1,
+          perPage: 2,
+          sort: 'name',
+          sortDir: 'asc',
+          filter: 'TEST',
+        }),
+      )
+      const searchOutputPage2 = await sut.search(
+        new SearchParams({
+          page: 2,
+          perPage: 2,
+          sort: 'name',
+          sortDir: 'asc',
+          filter: 'TEST',
+        }),
+      )
+
+      expect(searchOutputPage1.items[0].toJSON()).toMatchObject(
+        entities[0].toJSON(),
+      )
+      expect(searchOutputPage1.items[1].toJSON()).toMatchObject(
+        entities[4].toJSON(),
+      )
+      expect(searchOutputPage2.items[0].toJSON()).toMatchObject(
+        entities[2].toJSON(),
+      )
+    })
   })
 })
