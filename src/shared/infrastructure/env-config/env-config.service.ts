2e8afacaea1be4a91c9c1336d64983d88605ddf4
FelipeConsorte2001 <cfelipeconsorte2001@gmail.com>
2025-09-20 15:17:45 -0300

    implement auth module

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -4,7 +4,7 @@ import { EnvConfig } from './env-config.interface'
 
 @Injectable()
 export class EnvConfigService implements EnvConfig {
-  constructor(private configService: ConfigService) { }
+  constructor(private configService: ConfigService) {}
 
   getAppPort(): number {
     return Number(this.configService.get<number>('PORT'))
