69027dd0ed4ae90a01f8fa3e161609d5cd6252d5
FelipeConsorte2001 <cfelipeconsorte2001@gmail.com>
2025-10-11 13:47:06 -0300

    implement and config swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 import { applyGlobalConfig } from './global-config'
 async function bootstrap() {
@@ -10,6 +11,23 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   )
+  const config = new DocumentBuilder()
+    .setTitle('Node.js Course')
+    .setDescription(
+      'Node.js Rest API - NestJS, Typescripty, DDD, Clean Architecture and Automated Tests',
+    )
+    .setVersion('1.0.0')
+    .addBearerAuth({
+      description: 'Informar o JWT para autorizar o acesso ',
+      name: 'Authorization',
+      scheme: 'Bearer',
+      type: 'http',
+      in: 'Header',
+    })
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('doc', app, document)
 
   applyGlobalConfig(app)
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0')
